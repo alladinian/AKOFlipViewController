ccc5dc9beff8986faa713947e0b785b820153e2b
Vasilis Akoinoglou <alladinian@gmail.com>
2014-07-15 12:08:04 +0300

    Better handling of gestures

--- Pod/Classes/AKOFlipInteractiveTransition.m
+++ Pod/Classes/AKOFlipInteractiveTransition.m
@@ -7,10 +7,60 @@
 //
 
 #import "AKOFlipInteractiveTransition.h"
+#import <UIKit/UIGestureRecognizerSubclass.h>
+
+typedef NS_ENUM(NSUInteger, AKOPanGestureRecognizerDirection) {
+    AKOPanGestureRecognizerDirectionVertical,
+    AKOPanGestureRecognizerDirectionHorizontal
+};
+
 
 @interface AKOPanGestureRecognizer : UIPanGestureRecognizer
+{
+    BOOL _drag;
+    int _moveX;
+    int _moveY;
+}
+@property (nonatomic, assign) AKOPanGestureRecognizerDirection direction;
 @end
 @implementation AKOPanGestureRecognizer
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    [super touchesMoved:touches withEvent:event];
+    if (self.state == UIGestureRecognizerStateFailed) return;
+    CGPoint nowPoint = [[touches anyObject] locationInView:self.view];
+    CGPoint prevPoint = [[touches anyObject] previousLocationInView:self.view];
+    _moveX += prevPoint.x - nowPoint.x;
+    _moveY += prevPoint.y - nowPoint.y;
+    if (!_drag) {
+        if (fabs(_moveX) > fabs(_moveY)) {
+            if (_direction == AKOPanGestureRecognizerDirectionVertical) {
+                self.state = UIGestureRecognizerStateFailed;
+            }else {
+                _drag = YES;
+            }
+        } else if (fabs(_moveY) > fabs(_moveX)) {
+            if (_direction == AKOPanGestureRecognizerDirectionHorizontal) {
+                self.state = UIGestureRecognizerStateFailed;
+            }else {
+                _drag = YES;
+            }
+        }
+    }
+}
+
+- (void)reset {
+    [super reset];
+    _drag = NO;
+    _moveX = 0;
+    _moveY = 0;
+}
+
+@end
+
+
+@interface AKOFlipInteractiveTransition ()
+@property (nonatomic, assign) CGFloat percentage;
 @end
 
 @implementation AKOFlipInteractiveTransition
@@ -23,8 +73,19 @@
             [view removeGestureRecognizer:r];
         }
     }
-    UIPanGestureRecognizer *gesture = [[AKOPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
+    AKOPanGestureRecognizer *gesture = [[AKOPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
+    gesture.direction = (AKOPanGestureRecognizerDirection)self.transitionDirection;
     [self.view addGestureRecognizer:gesture];
+}
+
+- (void)setTransitionDirection:(AKOFlipTransitionDirection)transitionDirection
+{
+    _transitionDirection = transitionDirection;
+    for (AKOPanGestureRecognizer *r in self.view.gestureRecognizers) {
+        if ([r isKindOfClass:[AKOPanGestureRecognizer class]]) {
+            r.direction = (AKOPanGestureRecognizerDirection)self.transitionDirection;
+        }
+    }
 }
 
 - (void)handlePan:(AKOPanGestureRecognizer *)gesture
@@ -36,6 +97,8 @@
         case UIGestureRecognizerStateBegan:
         {
             NSLog(@"Gesture began...");
+            
+            self.percentage = 0.0f;
             
             CGPoint nowPoint = [gesture locationInView:self.view];
             CGFloat boundary = CGRectGetMidY(self.view.frame);
@@ -66,13 +129,13 @@
         {
             CGRect viewRect = self.view.bounds;
             CGPoint translation = [gesture translationInView:self.view];
-            CGFloat percent = isVertical ? (translation.y / viewRect.size.height) : (translation.x / viewRect.size.width);
-            percent = fabsf(percent);
-            percent = MIN(1.0, MAX(0.0, percent));
-            [self updateInteractiveTransition:percent];
+            _percentage = isVertical ? (translation.y / viewRect.size.height) : (translation.x / viewRect.size.width);
+            _percentage = fabsf(_percentage);
+            _percentage = MIN(1.0, MAX(0.0, _percentage));
+            [self updateInteractiveTransition:_percentage];
             
             NSLog(@"Gesture changed...");
-            NSLog(@"Translation: %@ | Percent: %@", NSStringFromCGPoint(translation), @(percent));
+            //NSLog(@"Translation: %@ | Percent: %@", NSStringFromCGPoint(translation), @(percent));
             
             break;
         }
@@ -84,22 +147,21 @@
         case UIGestureRecognizerStateEnded:
         {
             NSLog(@"Gesture Ended...");
-            CGPoint nowPoint = [gesture locationInView:self.view];
-            CGFloat boundary = isVertical ? (self.view.frame.origin.y + (self.view.frame.size.height / 2.0f)) :  (self.view.frame.origin.x + (self.view.frame.size.width / 2.0f));
-            if (self.isPresenting){
-                if (isVertical ? (boundary > nowPoint.y) : (boundary > nowPoint.x)) {
+            
+            const CGFloat threshold = 0.3f;
+            BOOL passedThreshold = _percentage > threshold;
+            
+            if (passedThreshold) {
+                if (self.isPresenting) {
                     [self finishInteractiveTransition];
                 } else {
-                    [self cancelInteractiveTransition];
-                }
-            } else {
-                if (isVertical ? (boundary < nowPoint.y) : (boundary < nowPoint.x)) {
                     [self finishInteractiveTransition];
                     [self.delegate completePopTransition];
-                }else{
-                    [self cancelInteractiveTransition];
                 }
+            } else {
+                [self cancelInteractiveTransition];
             }
+
             break;
         }
         default:
